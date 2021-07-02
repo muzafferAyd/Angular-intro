0f501822efd6944e555414772651c9410fd938c3
Muzaffer <aydogdum38@gmail.com>
2021-07-02 17:13:38 +0300

    added HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import {HttpClientModule} from "@angular/common/http"
+ 
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { CategoryComponent } from './category/category.component';
 import { NaviComponent } from './navi/navi.component';
 import { ProductFilterPipe } from './product/product-filter.pipe';
+import { AlertifyService } from './services/alertify.service';
 
 
 @NgModule({
@@ -19,9 +21,10 @@ import { ProductFilterPipe } from './product/product-filter.pipe';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AlertifyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,28 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from './product';
+import { AlertifyService } from '../services/alertify.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-
-  constructor() { }
-  title ="Ürün Listesi"
-  filterText = ""
-  products : Product[] = [
-    {id:1, name : "Laptop", price: 2500, categoryId : 1, description: "Asus Zen", imageUrl:"https://images.unsplash.com/photo-1554415707-6e8cfc93fe23?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"},
-    {id:2, name : "Mouse", price: 100, categoryId : 2, description: "A4 tech", imageUrl:"https://images.unsplash.com/photo-1554415707-6e8cfc93fe23?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"},
-    {id:1, name : "Keyboard", price: 2500, categoryId : 1, description: "Asus Zen", imageUrl:"https://images.unsplash.com/photo-1554415707-6e8cfc93fe23?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"},
-    {id:2, name : "Tv", price: 100, categoryId : 2, description: "A4 tech", imageUrl:"https://images.unsplash.com/photo-1554415707-6e8cfc93fe23?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"},
-
-  ]
+  constructor(
+    private alertifyService: AlertifyService,
+    private http: HttpClient
+  ) {}
+  title = 'Ürün Listesi';
+  filterText = '';
+  products: Product[];
+  path = 'http://localhost:3000/products';
   ngOnInit(): void {
+    this.http
+    .get<Product[]>(this.path)
+    .subscribe(data => {
+      this.products = data;
+    });
   }
 
-  addToCart(product){
-    alert("Sepete eklendi: "+ product.name)
+  addToCart(product) {
+    this.alertifyService.success('Sepete eklendi: ' + product.name);
   }
-
 }
