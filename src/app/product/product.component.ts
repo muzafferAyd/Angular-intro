1d0e00d6d134969081597d7d5a7dc3f50d27e911
Muzaffer <aydogdum38@gmail.com>
2021-07-02 17:49:13 +0300

    added observable

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,27 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from './product';
 import { AlertifyService } from '../services/alertify.service';
-import { HttpClient } from '@angular/common/http';
+import { ProductService } from '../services/product.service';
+
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css'],
+  providers:[ProductService]
 })
 export class ProductComponent implements OnInit {
   constructor(
     private alertifyService: AlertifyService,
-    private http: HttpClient
+    private productService:ProductService
   ) {}
   title = 'Ürün Listesi';
   filterText = '';
   products: Product[];
-  path = 'http://localhost:3000/products';
+
   ngOnInit(): void {
-    this.http
-    .get<Product[]>(this.path)
-    .subscribe(data => {
-      this.products = data;
+    this.productService.getProducts().subscribe(data=>{
+      this.products = data
     });
   }
 
